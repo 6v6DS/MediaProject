c67ee6665ab3f0b2ce6f86bd074ac77d255d6f42
6v6DS <142214903+6v6DS@users.noreply.github.com>
2024-05-16 14:37:54 +0900

    Create index.js
    동아리 가입 신청서

---
+++ DS/submit/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    show_submit();
+});
+
+document.querySelectorAll('input[name="grade"]').forEach((radio)=>{
+   radio.addEventLister('change', (event) => {
+    let selectedId = event.target.id;
+   }) 
+})
+
+
+function show_order() {
+    event.preventDefault();
+
+    //Get
+    let name = $('#name').val()
+    let studentID = $('#studentID').val()
+    let major = $('#major').val()
+    let phone = $('#phone').val()
+    let age = $('#age').val()
+    let bio = $('#bio').val()
+    let act = $('#act').val()
+    let selectedId = document.querySelector('input[name="grade"]:checked').id;
+
+    let formData = new FormData();
+    formData.append("name_give", name)
+    formData.append("studentID_give", studentID)
+    formData.append("selectedId_give", selectedId)
+    formData.append("major_give", major)
+    formData.append("phone_give", phone)
+    formData.append("age_give", age)
+    formData.append("bio_give", bio)
+    formData.append("act_give", act)
+
+    // Post
+    fetch('/application', { 
+        method: "POST", 
+        body: formData 
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        alert('신청이 완료되었습니다');
+        window.location.reload();
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+    return false;
+}
