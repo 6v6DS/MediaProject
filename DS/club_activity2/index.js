2057c0c4e07d3d814c09267ecb81083bafff90da
eosa3993 <eosa3993@gmail.com>
2024-05-26 06:01:21 +0900

    Add files via upload

--- DS/club_activity2/index.js
+++ DS/club_activity2/index.js
@@ -10,9 +10,18 @@
         blog.tab = 'blog';
 
         //여기에서, data 받아오고 넣어주세요 데이터..
-        $http.get('https://json')
+        $http.get('/posts')
             .then(function (response) {
                 blog.posts = response.data;
+                blog.posts.forEach(function(post) {
+                    $http.get('/comments/' + post.id)
+                        .then(function (response) {
+                            post.comments = response.data;
+                        })
+                        .catch(function (error) {
+                            console.error('Error:', error);
+                        });
+                });
             })
             .catch(function (error) {
                 console.error('Error:', error);
@@ -30,32 +39,27 @@
 
         //글 post 요청
         blog.addPost = function () {
-            blog.post.createdOn = Date.now();
-            blog.post.comments = [];
-            //form 데이터에 태워서 보내주기
-            let formData = new FormData();
-            formData.append("title", blog.post.title);
-            formData.append("body", blog.post.body.join('\n')); 
-            formData.append("image", blog.post.image);
-            formData.append("author", blog.post.author);
-            formData.append("createdOn", blog.post.createdOn);
-
-            //데이터전송
-            fetch('/contents', { method: "POST", body: formData })
-                .then((res) => res.json())
-                .then((data) => {
-                    alert(data['msg']);
+            var postData = {
+                title: blog.post.title,
+                body: blog.post.body.join('\n'),
+                image: blog.post.image,
+                author: blog.post.author,
+                createdOn: Date.now()
+            };
+        
+            $http.post('/posts', postData)
+                .then(function (response) {
+                    alert('게시물이 추가되었습니다.');
                     blog.posts.unshift(blog.post);
-                    blog.tab = 'blog';
                     blog.post = {};
-
-                    $scope.$apply();
+                    blog.tab = 'blog';
                 })
-                .catch((error) => {
+                .catch(function (error) {
                     console.error("Error:", error);
-                    alert("글 추가에 실패했습니다.");
+                    alert("글 추가에 실패했습니다");
                 });
-        }
+        };
+        
 
 
 
@@ -66,47 +70,24 @@
     //Post 받아오기.
     app.controller('CommentController', ['$http', function($http){
         var commentCtrl = this;
-        commentCtrl.comments = [];
-
-        //여기에 댓글 데이터 넣어주세요 흑흑
-        $http.get('/comments')
-        .then(function(response){
-            commentCtrl.comments = response.data;
-        })
-        .catch(function(error){
-            console.error("Error fetching comments:", error);
-            alert("댓글 데이터를 가져오는 데 실패했습니다.");
-        });
 
         commentCtrl.addComment = function(post){
-            // 새로운 댓글 생성
             var newComment = {
+                postId: post.id,
                 body: commentCtrl.comment.body,
                 author: commentCtrl.comment.author,
                 createdOn: Date.now()
             };
 
-
-            let formData = new FormData();
-            formData.append("postId", post.id); 
-            formData.append("body", newComment.body);
-            formData.append("author", newComment.author);
-            formData.append("createdOn", newComment.createdOn);
-
-
-            fetch('/comments', { method: "POST", body: formData })
-                .then((res) => res.json())
-                .then((data) => {
-                    alert(data['msg']);
-                    
-                    post.comments.push(newComment);
+            $http.post('/comments', newComment)
+                .then(function (response) {
+                    alert('댓글이 추가되었습니다.');
+                    post.comments.push(response.data);
                     commentCtrl.comment = {};
-                    
-                    $scope.$apply();
                 })
-                .catch((error) => {
+                .catch(function (error) {
                     console.error("Error:", error);
-                    alert("댓글 추가에 실패했습니다.");
+                    alert("댓글 추가에 실패했습니다");
                 });
         };
     }]);
